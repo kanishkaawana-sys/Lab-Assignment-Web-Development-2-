c112b910febb02a95979bff7fc38e2914ea9d54c
kanishkaawana-sys <kanishkaawana123@gmail.com>
2026-04-18 18:09:34 +0530

    Add files via upload

---
+++ To-do App/To- do app.js
@@ -0,0 +1,101 @@
+// const inputBox = document.querySelector('inputBox')
+// const addBtn = document.querySelector('#addBtn')
+// const list = document.querySelector('#list')
+
+
+// addBtn.addEventListener('click',()=>{
+//     let listData = []
+
+//     if(inputBox !== '') {
+
+//         listData.push({
+//             listValue:inputBox.value
+//         })
+
+//         console.log(listData)
+
+//         listData.forEach({element,i}=>{
+//             console.log(element,i)
+//         })
+//     }
+
+//     const eachList = documentFragment.createElement('div')
+//     eachList.innerHTML = `<p>${inputBox.value}</p> <button>Delete</button> <button>Edit</button>`
+//     list.append(eachList) 
+//  console.log(eachList)
+// })
+
+
+
+const inputBox = document.querySelector('#inputBox')
+const addBtn = document.querySelector('#addBtn')
+const list = document.querySelector('#list')
+
+
+let listData = []
+
+function createList() {
+    list.innerHTML = ''
+
+    listData.forEach((element, i) => {
+        // creating div
+        const eachList = document.createElement('div')
+        // inserting data into div
+        eachList.innerHTML = ` <p>${element.listValue}</p>
+                <button onclick='DeleteHandler(${i})'>Delete</button>
+                <button onclick='EditHandler(${i})'>Edit</button>`
+        list.append(eachList)
+        eachList.setAttribute('class', 'eachList')
+    })
+
+    inputBox.value = ''
+}
+
+
+let editState = null
+
+addBtn.addEventListener('click', () => {
+    if (inputBox.value !== '' && editState === null) {
+
+        listData.push({
+            listValue: inputBox.value
+        })
+       
+        inputBox.value = ''
+        // console.log(listData)   
+    }else{
+
+        listData[editState].listValue = inputBox.value
+        editState = null
+        addBtn.textContent = 'Add'
+
+
+    }
+
+     createList()
+})
+
+
+function DeleteHandler(i) {
+    listData.splice(i, 1)
+    createList()
+
+}
+
+function EditHandler(i){
+
+    inputBox.value = listData[i].listValue
+    addBtn.textContent = 'Update'
+    editState = i
+    // listData[i].listValue = 
+
+
+}
+
+
+// let arr = [1,2,3,4]
+
+// arr.splice(1,1)
+// console.log(arr)
+
+let x= prompt('enter')
