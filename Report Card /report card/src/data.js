a8c9ab0525ac0f3fdb55c826571eff846f63049f
kanishkaawana-sys <kanishkaawana123@gmail.com>
2026-05-02 17:12:06 +0530

    Add files via upload

---
+++ Report Card /report card/src/data.js
@@ -0,0 +1,24 @@
+const report = [
+  {
+    id: 1,
+    name: "Shashwat",
+    score: 100
+  },
+  {
+    id: 2,
+    name: "Reyansh",
+    score: 90
+  },
+  {
+    id: 3,
+    name: "Siya",
+    score: 30
+  },
+  {
+    id: 4,
+    name: "Arushi",
+    score: 28
+  }
+];
+
+export default report;
